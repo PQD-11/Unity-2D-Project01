8affc4ebe739133197aa9b0b0f0c40a4b18b2f43
Dang Pham <85118356+PQD-11@users.noreply.github.com>
2023-08-03 00:26:07 +0700

    [add] lives score Text

--- TileVania/Assets/Scripts/GameSession.cs
+++ TileVania/Assets/Scripts/GameSession.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+    [SerializeField] TextMeshProUGUI liveText;
+    [SerializeField] TextMeshProUGUI scoreText;
     void Awake() 
     {
         int numGameSessions = FindObjectsOfType<GameSession>().Length;
@@ -17,6 +21,10 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    void Start() {
+        liveText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
     public void ProcessPlayerDeath(){
         if (playerLives > 1){
             TakeLife();
@@ -26,10 +34,17 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
     void TakeLife()
     {
         playerLives--;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        liveText.text = playerLives.ToString();
     }
 
     void ResetGameSession()
